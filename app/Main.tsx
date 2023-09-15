25e391eb231f50c620bc0c595446458a78c35bc0
SC-SoftwareHS <harrison@sylvanitecapital.com>
2023-09-15 09:54:06 -0700

    update main

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,13 +1,13 @@
 const MainPage: React.FC = () => {
   return (
     <div style={{ textAlign: 'left', paddingTop: '50px' }}>
-      <h1 style={{ color: '#0e3e67', fontSize: '36px', fontWeight: 'bold' }}>
+      <h1 style={{ color: '#0e3e67', fontSize: '50px', fontWeight: 'bold' }}>
         Immediate Active Threat Response
       </h1>
       <h2
         style={{
-          textAlign: 'center',
-          paddingTop: '40px',
+          textAlign: 'left',
+          paddingTop: '35',
           color: '#0e3e67',
           fontSize: '40px',
           fontWeight: 'bold',
@@ -15,17 +15,16 @@ const MainPage: React.FC = () => {
       >
         Improve Your Safety Without Spending a Fortune.
       </h2>
-      <p style={{ textAlign: 'left', paddingTop: '10px', color: '#0e3e67', fontSize: '30px'}}>
-      - Immediate crisis alerts and communication <br />
-      - Simple immediate alert activation alert uses Amber Alert style alarm <br />
+      <p style={{ textAlign: 'left', paddingTop: '10px', color: '#0e3e67', fontSize: '30px' }}>
+        - Immediate crisis alerts and communication <br />
+        - Simple immediate alert activation alert uses Amber Alert style alarm <br />
         - Customizable alert includes location of threat <br />
-      - Includes SRO's and police for faster response <br />
+        - Includes SRO's and police for faster response <br />
         - Live messaging to share crisis updates
         <br />
         - Easily accommodates multiple campuses
         <br />- Affordable!
       </p>
-
     </div>
   )
 }
